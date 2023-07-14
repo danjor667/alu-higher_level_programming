2cb89c02f5723470eecd35a7c3965c545e639814
danjor667 <jordannguepi@gnail.com>
2023-07-14 10:30:33 +0000

    data structures

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    str = ""
+    for i in range(len(my_string)):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        else:
+            str += my_string[i]
+    return str
---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    else:
+        for row in matrix:
+            for j in range(len(row)):
+                if j < len(row) - 1:
+                    print("{:d}".format(row[j]), end=" ")
+                else:
+                    print("{:d}".format(row[j]))
