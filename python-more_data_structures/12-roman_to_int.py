d0823f788456aa450bf0a676b2f6b5469f877677
danjor667 <jordannguepi@gnail.com>
2023-07-14 13:51:22 +0000

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,7 +13,7 @@ def roman_to_int(roman_string):
         int equivilent of roman_string
     """
 
-    dic = {"I": 1, "V": 5, "X": 10, "C": 100, "D": 500, "M": 1000}
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     value = 0
     if roman_string is None or type(roman_string) != str:
         return value
