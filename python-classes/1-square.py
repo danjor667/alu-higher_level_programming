246ccab65dfa36b05d111b7368e1c2df8c2260b3
danjor667 <j.nguepi@alustudent.com>
2023-07-21 05:09:20 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+defineing a class Square
+"""
+
+
+class Square():
+    """
+    class Square
+    """
+    def __init__(self, size):
+        """
+        initializing an instance
+        """
+        self.__size = size
