e77aef3909fb35bdeb98930e7077c16b48bb025a
danjor667 <jordannguepi@gnail.com>
2023-07-23 13:40:24 +0000

    updates: file 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,6 +15,7 @@ class Square(Rectangle):
         """
         super().integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """
