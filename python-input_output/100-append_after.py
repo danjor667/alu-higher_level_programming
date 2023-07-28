f16455e7200bd45f53271b7168a3f3e63df2d2cd
danjor667 <jordannguepi@gnail.com>
2023-07-28 19:07:37 +0000

    task 100

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -12,8 +12,9 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename) as f:
         lines = f.readlines()
-    for idx, line in enumerate(lines):
-        if search_string in line:
-            lines.insert(idx + 1, new_string)
+    for i in range(len(lines)):
+        if search_string in lines[i]:
+            lines.insert(i + 1, new_string)
     with open(filename, "w") as f:
-        f.write("".join(lines))
+        for line in lines
+        f.write(line + "\n")
