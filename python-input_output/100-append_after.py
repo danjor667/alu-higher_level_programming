9d8c96d9cc7d9d26b511945df1cba037d227ea18
danjor667 <jordannguepi@gnail.com>
2023-07-28 20:29:52 +0000

    task 100

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -10,13 +10,11 @@ def append_after(filename="", search_string="", new_string=""):
     add a str after a line
     containing a specifique character
     """
+    data = ""
     with open(filename) as f:
-        lines = f.readlines()
-    for i in range(len(lines)):
-        if search_string in lines[i]:
-            if i + 1 >= len(lines):
-                lines.append(new_string)
-            lines.insert(i + 1, new_string)
+        for line in f:
+            data += line
+            if search_string in line:
+                data += search_string
     with open(filename, "w") as f:
-        for line in lines:
-            f.write(line)
+            f.write(data)
