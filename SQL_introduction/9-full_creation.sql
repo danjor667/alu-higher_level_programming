3d64100a385fc282e602292bec657f73b71a4f62
danjor667 <j.nguepi@alustudent.com>
2023-07-31 10:44:33 -0700

    retry task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creating a new table and inserting new records
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score)
 VALUE(
 	1, "John", 10)
