7d76185693453ad678807b3f4e5a2723d4eee539
danjor667 <j.nguepi@alustudent.com>
2023-07-31 14:26:32 -0700

    redo tak o

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- showing the priviledge of 2 users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- showing the priviledges of 2 users
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
