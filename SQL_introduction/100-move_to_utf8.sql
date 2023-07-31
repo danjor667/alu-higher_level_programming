a6075856da21e8cec5869a54d722a0a66bccecae
danjor667 <j.nguepi@alustudent.com>
2023-07-31 11:53:41 -0700

    redo task 100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- converting the database and table to utf8
 USE hbtn_0c_0;
-ALTER hbtn_0c_0 CHARACTER SET utf8mb4, COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4, COLLATE utf8mb4_unicode_ci;
+ALTER hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
