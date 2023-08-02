61a59844e82ae992ca8b08faa37e8e7b34409e37
danjor667 <j.nguepi@alustudent.com>
2023-08-02 05:51:13 -0700

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- creating a database and a table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	state_id INT  NOT NULL, FOREIGN KEY(state_id)REFERENCE state(id),
+	name VARCHAR(256) NOT NULL);
