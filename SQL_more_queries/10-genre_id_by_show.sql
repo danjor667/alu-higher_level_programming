c646df8ad6629dcfc0a3205ed18a3057ecfe0f0f
danjor667 <j.nguepi@alustudent.com>
2023-08-03 03:44:14 -0700

    task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- selecting  all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_show.title, tv_show_genre.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER  JOIN tv_show_genres
-ON tv_show_id = tv_show_genre_id
-ORDER BY tv_show_genres.genre_id
+ON tv_shows_id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
