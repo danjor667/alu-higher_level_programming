7172d2ef09df660d4fc0175d8db2a8520506bbb9
danjor667 <j.nguepi@alustudent.com>
2023-08-03 03:35:06 -0700

    task 10 retry

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
-/* selecting  all shows contained in hbtn_0d_tvshows
- that have at least one genre linked */
+-- selecting  all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_show.title, tv_show_genre.genre_id
 FROM tv_shows INNER  JOIN tv_show_genres
 ON tv_show_id = tv_show_genre_id
