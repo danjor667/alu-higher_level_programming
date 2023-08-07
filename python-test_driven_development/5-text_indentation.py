6d81b5e75a1a5ad1240e2738be62fc787c793e62
danjor667 <jordannguepi@gmail.com>
2023-08-07 22:52:07 +0000

    task

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -20,7 +20,7 @@ def text_indentation(text):
             current_line = ""
     lines.append(current_line.strip())
     for line in lines:
-        if line = lines[len(lines) - 1]:
+        if line == lines[len(lines) - 1]:
             print(line, end="")
         else:
             print(line + "\n")
