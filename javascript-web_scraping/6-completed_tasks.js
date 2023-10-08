1c4331dcee482fabd35892151fc5fc1eff42cd5c
danjor667 <jordannguepi@gmail.com>
2023-10-08 13:54:38 +0200

    task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length === 3) {
+  const url = process.argv[2];
+  request.get(url, (error, response) => {
+    if (!error) {
+      response = JSON.parse(response.body);
+      const dict = {};
+      for (const ele of response) {
+        const key = ele.userId;
+        if (key in dict && ele.completed === true) {
+          dict[key] += 1;
+        } else if (key in dict && ele.completed === false) {
+          continue;
+        } else {
+          dict[key] = ele.completed ? 1 : 0;
+        }
+      }
+      console.log(dict);
+    }
+  });
+}
