eadd6728a6d2051ea9d258000ca7729043feae88
danjor667 <jordannguepi@gmail.com>
2023-10-08 12:33:05 +0200

    fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,21 +3,21 @@
 const request = require('request');
 const fs = require('fs');
 
+function get (url, filePath) {
+  request.get(url, (error, response) => {
+    if (!error) {
+      response = response.body;
+      fs.writeFile(filePath, response, (error) => {
+        if (error) {
+          console.log(`${error}`);
+        }
+      });
+    }
+  });
+}
+
 if (process.argv.length === 4) {
   const url = process.argv[2];
   const filePath = process.argv[3];
-
-  function get () {
-    request.get(url, (error, response) => {
-      if (!error) {
-        response = response.body;
-        fs.writeFile(filePath, response, (error) => {
-          if (error) {
-            console.log(`${error}`);
-          }
-        });
-      }
-    });
-  }
-  get();
+  get(url, filePath);
 }
